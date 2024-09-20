9dfae90199041ff7ce80102e18848101865a4b04
Andres Fernando Corani Rodriguez <Andre@MacBook-Pro-de-Andres-Corani.local>
2024-09-19 22:04:30 -0400

    vercel start PWA

--- vite.config.js
+++ vite.config.js
@@ -6,72 +6,35 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "favicon.svg",
-        "robots.txt",
-        "apple-touch-icon.png",
-        "masked-icon.svg"
-      ],
       manifest: {
-        name: "Mi App PWA",
-        short_name: "MiApp",
-        description: "Descripción de mi aplicación PWA",
-        theme_color: "#ffffff",
         icons: [
           {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          },
-          {
-            src: "pwa-512x512.png",
+            src: "/512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable"
-          }
-        ]
+          },
+        ],
       },
       workbox: {
         runtimeCaching: [
           {
-            urlPattern: ({ url }) =>
-              url.origin === "http://localhost:3000" &&
-              url.pathname.startsWith("/api/"),
-            handler: "NetworkFirst",
+            urlPattern: ({ url }) => {
+              if (url.protocol === "http:" || url.protocol === "https:") {
+                return true;
+              }
+              return false;
+            },
+            handler: "CacheFirst" /* or 'StaleWhileRevalidate' */,
             options: {
               cacheName: "api-cache",
-              networkTimeoutSeconds: 10,
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 // 1 día
-              },
               cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
+                statuses: [200],
+              },
+            },
           },
-          {
-            urlPattern: ({ url }) =>
-              url.origin === self.location.origin &&
-              !url.pathname.startsWith("/api/"),
-            handler: "StaleWhileRevalidate",
-            options: {
-              cacheName: "assets-cache",
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 24 * 7 // 1 semana
-              }
-            }
-          }
         ]
-      }
-    })
+      },
+    }),
   ],
   build: {
     target: 'esnext', // This enables top-level await
